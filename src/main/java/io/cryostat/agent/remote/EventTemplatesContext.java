06a8cc10f24feba30c9db3ae31f1b31f29f1b428
Andrew Azores <aazores@redhat.com>
2023-07-27 16:10:08 -0400

    feat(api): enable dynamic JFR start (#165)

--- src/main/java/io/cryostat/agent/remote/EventTemplatesContext.java
+++ src/main/java/io/cryostat/agent/remote/EventTemplatesContext.java
@@ -48,30 +48,32 @@ class EventTemplatesContext implements RemoteContext {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String mtd = exchange.getRequestMethod();
-        switch (mtd) {
-            case "GET":
-                try {
-                    exchange.sendResponseHeaders(HttpStatus.SC_OK, 0);
-                    try (OutputStream response = exchange.getResponseBody()) {
-                        FlightRecorderMXBean bean =
-                                ManagementFactory.getPlatformMXBean(FlightRecorderMXBean.class);
-                        List<String> xmlTexts =
-                                bean.getConfigurations().stream()
-                                        .map(ConfigurationInfo::getContents)
-                                        .collect(Collectors.toList());
-                        mapper.writeValue(response, xmlTexts);
+        try {
+            String mtd = exchange.getRequestMethod();
+            switch (mtd) {
+                case "GET":
+                    try {
+                        exchange.sendResponseHeaders(HttpStatus.SC_OK, 0);
+                        try (OutputStream response = exchange.getResponseBody()) {
+                            FlightRecorderMXBean bean =
+                                    ManagementFactory.getPlatformMXBean(FlightRecorderMXBean.class);
+                            List<String> xmlTexts =
+                                    bean.getConfigurations().stream()
+                                            .map(ConfigurationInfo::getContents)
+                                            .collect(Collectors.toList());
+                            mapper.writeValue(response, xmlTexts);
+                        }
+                    } catch (Exception e) {
+                        log.error("events serialization failure", e);
                     }
-                } catch (Exception e) {
-                    log.error("events serialization failure", e);
-                } finally {
-                    exchange.close();
-                }
-                break;
-            default:
-                exchange.sendResponseHeaders(HttpStatus.SC_NOT_FOUND, -1);
-                exchange.close();
-                break;
+                    break;
+                default:
+                    log.warn("Unknown request method {}", mtd);
+                    exchange.sendResponseHeaders(HttpStatus.SC_METHOD_NOT_ALLOWED, -1);
+                    break;
+            }
+        } finally {
+            exchange.close();
         }
     }
 }
