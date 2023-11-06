19a3eefdcda781c290fad185810415cbd46b43c7
Andrew Azores <aazores@redhat.com>
2023-11-06 12:29:30 -0500

    feat(baseuri): add baseuri-range config property to control servers the Agent will register with

--- src/main/java/io/cryostat/agent/util/StringUtils.java
+++ src/main/java/io/cryostat/agent/util/StringUtils.java
@@ -21,4 +21,8 @@ public class StringUtils {
     public static boolean isBlank(String s) {
         return s == null || s.isBlank();
     }
+
+    public static boolean isNotBlank(String s) {
+        return !isBlank(s);
+    }
 }
