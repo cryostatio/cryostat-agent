26db8af753ffc5796db1fd75920f0fb872e26ff4
Andrew Azores <aazores@redhat.com>
2024-04-29 12:24:27 -0400

    check size after dumping to file in case in-memory buffer has not been flushed yet

--- src/main/java/io/cryostat/agent/harvest/Harvester.java
+++ src/main/java/io/cryostat/agent/harvest/Harvester.java
@@ -348,13 +348,13 @@ public class Harvester implements FlightRecorderListener {
     private Future<Void> uploadOngoing(PushType pushType, RecordingSettings settings) {
         Recording recording = settings.apply(flightRecorder.takeSnapshot());
         try {
-            if (recording.getSize() < 1) {
-                return CompletableFuture.failedFuture(
-                        new IllegalStateException("No source recording data"));
-            }
             Path exitPath = Files.createTempFile(null, null);
             Files.write(exitPath, new byte[0], StandardOpenOption.TRUNCATE_EXISTING);
             recording.dump(exitPath);
+            if (Files.size(exitPath) < 1) {
+                return CompletableFuture.failedFuture(
+                        new IllegalStateException("No source recording data"));
+            }
             log.trace("Dumping {}({}) to {}", recording.getName(), recording.getId(), exitPath);
             return client.upload(pushType, sownRecording, maxFiles, exitPath)
                     .thenRun(
