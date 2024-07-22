5066db5409406b52817ed20e95361be0a7e89bf1
Andrew Azores <aazores@redhat.com>
2024-07-22 15:46:58 -0400

    fix(token): retry request if token may have potentially expired between reading from fs and handling by remote authenticator

--- src/main/java/io/cryostat/agent/ConfigModule.java
+++ src/main/java/io/cryostat/agent/ConfigModule.java
@@ -245,7 +245,7 @@ public abstract class ConfigModule {
 
     @Provides
     @Singleton
-    @Named(CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_TIMEOUT_MS)
+    @Named(CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_RETRY_COUNT)
     public static int provideCryostatAgentWebclientResponseRetryCount(Config config) {
         return config.getValue(CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_RETRY_COUNT, int.class);
     }
