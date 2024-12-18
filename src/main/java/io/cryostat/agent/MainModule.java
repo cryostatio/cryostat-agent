9b1d2a80476004509f17802760e38c9f85c72ab5
jmatsuok <jmatsuok@redhat.com>
2024-12-17 21:11:04 -0500

    Enforce TLS by default, add new configuration property to control this

--- src/main/java/io/cryostat/agent/MainModule.java
+++ src/main/java/io/cryostat/agent/MainModule.java
@@ -236,10 +236,20 @@ public abstract class MainModule {
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_TLS_CLIENT_AUTH_KEY_PASS_CHARSET)
                     String clientAuthKeyPassFileCharset,
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_TLS_CLIENT_AUTH_KEY_MANAGER_TYPE)
-                    String clientAuthKeyManagerType) {
+                    String clientAuthKeyManagerType,
+            @Named(ConfigModule.CRYOSTAT_AGENT_BASEURI) URI baseUri,
+            @Named(ConfigModule.CRYOSTAT_AGENT_TLS_ENABLED) boolean tlsEnabled) {
         try {
             KeyManager[] keyManagers = null;
-            if (clientAuthCertPath.isPresent() && clientAuthKeyPath.isPresent()) {
+            if (clientAuthCertPath.isPresent() && clientAuthKeyPath.isPresent() && tlsEnabled) {
+                if (!baseUri.getScheme().contains("https")) {
+                    throw new IllegalArgumentException(
+                            String.format(
+                                    "If TLS is enabled via the (%s) property, the base URI (%s)"
+                                            + " must be an https connection.",
+                                    ConfigModule.CRYOSTAT_AGENT_TLS_ENABLED,
+                                    ConfigModule.CRYOSTAT_AGENT_BASEURI));
+                }
                 KeyStore ks = KeyStore.getInstance(clientAuthKeystoreType);
                 Optional<CharBuffer> keystorePass =
                         readPass(
@@ -302,13 +312,17 @@ public abstract class MainModule {
                     clearBuffer(keystorePass);
                     clearBuffer(keyPass);
                 }
-            } else if (clientAuthCertPath.isPresent() || clientAuthKeyPath.isPresent()) {
+            } else if (clientAuthCertPath.isPresent()
+                    || clientAuthKeyPath.isPresent()
+                    || tlsEnabled) {
                 throw new IllegalArgumentException(
                         String.format(
                                 "To use TLS client authentication, both the certificate (%s) and"
-                                        + " private key (%s) properties must be set.",
+                                    + " private key (%s) properties must be set. The (%s) property"
+                                    + " must be true as well.",
                                 ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_TLS_CLIENT_AUTH_CERT_PATH,
-                                ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_TLS_CLIENT_AUTH_KEY_PATH));
+                                ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_TLS_CLIENT_AUTH_KEY_PATH,
+                                ConfigModule.CRYOSTAT_AGENT_TLS_ENABLED));
             }
 
             X509TrustManager trustManager = null;
@@ -610,18 +624,28 @@ public abstract class MainModule {
                     boolean verifyHostname,
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_CONNECT_TIMEOUT_MS) int connectTimeout,
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_TIMEOUT_MS) int responseTimeout,
-            @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_RETRY_COUNT) int retryCount) {
+            @Named(ConfigModule.CRYOSTAT_AGENT_WEBCLIENT_RESPONSE_RETRY_COUNT) int retryCount,
+            @Named(ConfigModule.CRYOSTAT_AGENT_TLS_ENABLED) boolean tlsEnabled) {
         SSLConnectionSocketFactory sslSocketFactory =
                 new SSLConnectionSocketFactory(
                         sslContext,
                         verifyHostname
                                 ? SSLConnectionSocketFactory.getDefaultHostnameVerifier()
                                 : NoopHostnameVerifier.INSTANCE);
-        Registry<ConnectionSocketFactory> socketFactoryRegistry =
-                RegistryBuilder.<ConnectionSocketFactory>create()
-                        .register("https", sslSocketFactory)
-                        .register("http", new PlainConnectionSocketFactory())
-                        .build();
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry;
+        if (tlsEnabled) {
+            socketFactoryRegistry =
+                    RegistryBuilder.<ConnectionSocketFactory>create()
+                            .register("https", sslSocketFactory)
+                            .build();
+        } else {
+            socketFactoryRegistry =
+                    RegistryBuilder.<ConnectionSocketFactory>create()
+                            .register("https", sslSocketFactory)
+                            .register("http", new PlainConnectionSocketFactory())
+                            .build();
+        }
         HttpClientConnectionManager connMan =
                 new BasicHttpClientConnectionManager(socketFactoryRegistry);
         return HttpClients.custom()
@@ -674,10 +698,11 @@ public abstract class MainModule {
             ScheduledExecutorService executor,
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBSERVER_HOST) String host,
             @Named(ConfigModule.CRYOSTAT_AGENT_WEBSERVER_PORT) int port,
-            @Named(HTTP_SERVER_SSL_CTX) Optional<SSLContext> sslContext) {
+            @Named(HTTP_SERVER_SSL_CTX) Optional<SSLContext> sslContext,
+            @Named(ConfigModule.CRYOSTAT_AGENT_TLS_ENABLED) boolean tlsEnabled) {
         try {
             HttpServer http;
-            if (sslContext.isEmpty()) {
+            if (sslContext.isEmpty() || !tlsEnabled) {
                 http = HttpServer.create(new InetSocketAddress(host, port), 0);
             } else {
                 http = HttpsServer.create(new InetSocketAddress(host, port), 0);
