a39aa8b9e2aa52f983229e5b3a2a530e0f8bdeaf
Andrew Azores <me@andrewazor.es>
2026-01-07 14:28:24 -0500

    fixup! active profile duration timestamp bugfix

--- src/main/java/io/cryostat/agent/remote/AsyncProfilerContext.java
+++ src/main/java/io/cryostat/agent/remote/AsyncProfilerContext.java
@@ -321,15 +321,15 @@ class AsyncProfilerContext extends MutatingRemoteContext {
             }
             String id = idGenerator.next();
             this.currentProfile = req;
-            // incoming request expects duration specified in seconds, but we use epoch millis for
-            // start timestamp and completed file content duration
-            this.currentProfile.duration =
-                    TimeUnit.SECONDS.convert(this.currentProfile.duration, TimeUnit.MILLISECONDS);
             this.currentProfile.id = id;
             this.scheduler.schedule(
                     () -> AsyncProfilerContext.this.currentProfile = null,
                     req.duration,
                     TimeUnit.SECONDS);
+            // incoming request expects duration specified in seconds, but we use epoch millis for
+            // start timestamp and completed file content duration
+            this.currentProfile.duration =
+                    TimeUnit.SECONDS.convert(this.currentProfile.duration, TimeUnit.MILLISECONDS);
             Path profile = repository.resolve(this.currentProfile.id);
             Files.deleteIfExists(profile);
             String events =
