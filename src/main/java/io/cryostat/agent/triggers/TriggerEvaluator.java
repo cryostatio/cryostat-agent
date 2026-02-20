a3eb9423666cb39f433b9a8e4c9cfc07494ad1ff
jmatsuok <jmatsuok@redhat.com>
2026-02-19 20:11:05 -0500

    Call sync endpoint when triggers conclude

--- src/main/java/io/cryostat/agent/triggers/TriggerEvaluator.java
+++ src/main/java/io/cryostat/agent/triggers/TriggerEvaluator.java
@@ -27,6 +27,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import io.cryostat.agent.CryostatClient;
 import io.cryostat.agent.FlightRecorderHelper;
 import io.cryostat.agent.harvest.Harvester;
 import io.cryostat.agent.model.MBeanInfo;
@@ -60,6 +61,7 @@ public class TriggerEvaluator {
     private final ConcurrentHashMap<String, SmartTrigger> triggers = new ConcurrentHashMap<>();
     private Future<?> task;
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final CryostatClient client;
 
     @SuppressFBWarnings("EI_EXPOSE_REP2")
     public TriggerEvaluator(
@@ -69,7 +71,8 @@ public class TriggerEvaluator {
             TriggerParser parser,
             FlightRecorderHelper flightRecorderHelper,
             Harvester harvester,
-            long evaluationPeriodMs) {
+            long evaluationPeriodMs,
+            CryostatClient client) {
         this.scheduler = scheduler;
         this.definitions = Collections.unmodifiableList(definitions);
         this.parser = parser;
@@ -77,6 +80,7 @@ public class TriggerEvaluator {
         this.flightRecorderHelper = flightRecorderHelper;
         this.harvester = harvester;
         this.evaluationPeriodMs = evaluationPeriodMs;
+        this.client = client;
     }
 
     public void start(String args) {
@@ -174,6 +178,7 @@ public class TriggerEvaluator {
                         if (t.isSimple() && evaluateTriggerConstraint(t, t.getTargetDuration())) {
                             log.trace("Trigger {} satisfied, starting recording...", t);
                             startRecording(t);
+                            client.syncSmartTrigger(t.getID());
                         } else if (!t.isSimple()) {
                             if (evaluateTriggerConstraint(t, Duration.ZERO)) {
                                 // Condition was met, set the state accordingly
@@ -192,6 +197,7 @@ public class TriggerEvaluator {
                         if (evaluateTriggerConstraint(t, Duration.ofMillis(difference))) {
                             log.trace("Trigger {} satisfied, completing...", t);
                             startRecording(t);
+                            client.syncSmartTrigger(t.getID());
                         } else if (evaluateTriggerConstraint(t, Duration.ZERO)) {
                             log.trace("Trigger {} satisfied, waiting for duration...", t);
                         } else {
